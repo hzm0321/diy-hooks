acf84e3c5999310ae5a0538b404e9df64346f9eb
hzm <934585316@qq.com>
2021-09-11 18:57:03 +0800

    feat: 静态地址

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/diy-hooks',
+  publicPath: '/diy-hooks/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   title: 'diy-hooks',
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
