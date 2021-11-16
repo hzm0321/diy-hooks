46225e4ecf444d8d77ab5572239571cf73eed7eb
hzm <934585316@qq.com>
2021-11-16 17:16:38 +0800

    feat: 补充 useMount 测试用例

--- src/hooks/src/useMount/__tests__/index.test.ts
+++ src/hooks/src/useMount/__tests__/index.test.ts
@@ -1,17 +1,25 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useMount from '../index';
 
+const fn = jest.fn();
+
 describe('useMount', () => {
-  it('should be defined', () => {
-    expect(useMount).toBeDefined();
+  it('挂载阶段调用一次', () => {
+    renderHook(() => useMount(fn));
+    expect(fn).toBeCalledTimes(1);
   });
-  it('test mount', async () => {
-    const fn = jest.fn();
-    const hook = renderHook(() => useMount(fn));
+
+  it('卸载阶段不会被调用', () => {
+    const { unmount } = renderHook(() => useMount(fn));
     expect(fn).toBeCalledTimes(1);
-    hook.rerender();
+    unmount();
     expect(fn).toBeCalledTimes(1);
-    hook.unmount();
+  });
+
+  it('重新渲染阶段不会被调用', () => {
+    const { rerender } = renderHook(() => useMount(fn));
+    expect(fn).toBeCalledTimes(1);
+    rerender();
     expect(fn).toBeCalledTimes(1);
   });
 });
